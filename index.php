b54a67d2c8b49637ed996b5253c3f5d0a1fbd62d
elaneg <elane.grd@gmail.com>
2023-11-20 15:34:05 +0100

    main : mise au propre

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -10,20 +9,26 @@
 <body>
 
 <h2>Status</h2>
+
 <h3>Temperature</h3>
+
 <ul id="temperature"></ul>
 <h3>Uptime</h3>
+
 <ul id="uptime"></ul>
 <h3>WiFi</h3>
+
 <ul id="wifi"></ul>
 <h3>Cloud</h3>
+
 <ul id="cloud"></ul>
+
 <button onclick="rebootDevice()">Reboot</button>
 <button onclick="toggleDevice()">Turn on/off</button>
 
 <script src="app.js"></script>
-</body>
 
+</body>
 
 </html>
 
