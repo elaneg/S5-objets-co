d8072de337606bd91ccfc82bce7c47efca1f2c6f
elaneg <elane.grd@gmail.com>
2023-11-20 15:28:18 +0100

    feat/interface : mise en place de l'interface et des connexions à la priseù

--- index.php
+++ index.php
@@ -4,13 +4,24 @@
 
 <head>
     <title>Plug S Shelly</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
-<h1>INTERFACE</h1>
 
+<h2>Status</h2>
+<h3>Temperature</h3>
+<ul id="temperature"></ul>
+<h3>Uptime</h3>
+<ul id="uptime"></ul>
+<h3>WiFi</h3>
+<ul id="wifi"></ul>
+<h3>Cloud</h3>
+<ul id="cloud"></ul>
+<button onclick="rebootDevice()">Reboot</button>
+<button onclick="toggleDevice()">Turn on/off</button>
 
-
+<script src="app.js"></script>
 </body>
 
 
